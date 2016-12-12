d91e5ffa25d810fd38d1fd56c00f099cec0aaafe
Stuart Sackler <stuart.sackler@gmail.com>
2016-12-12 16:40:50 -0500

    reduced the number of monetary exchange instances to 1 from 3 and changed the data pull to base on GBP and pull usd can and euro

--- src/services/currencyRatesData.js
+++ src/services/currencyRatesData.js
@@ -1,7 +1,8 @@
 app.factory('currencyRatesData', function($http){
   var factory = {};
   factory.getRates = function(){
-    return $http.get('data/rates.json');
+    //return $http.get('data/rates.json');
+    return $http.get('http://api.fixer.io/latest?base=GBP&symbols=USD,CAD,EUR');
   };
   return factory;
 });
